da91764668127078258dcfee68565d5ab16a1e33
k-e-l-ly <73137367+k-e-l-ly@users.noreply.github.com>
2022-07-29 15:39:32 +0300

    Update conn.php

--- VocationalWorkerSystem/conn.php
+++ VocationalWorkerSystem/conn.php
@@ -1,8 +1,8 @@
 <?php
-$Server = "Localhost";
+$db_server = "Localhost";
 $username = "root";
 $password = "";
 $db = "vocational_workers";
 
 
-$sql = mysqli_connect($Server, $username, $password, $db);
+$sql = mysqli_connect($db_server, $username, $password, $db);
